f9d6c687fc6a9a805ad50dee09b58adfcce10e2d
Lev Popov <popovle1@fit.cvut.cz>
2022-02-07 17:50:56 +0100

    Implemented functions for working with matrices.

--- popovle1_sudoku.rkt
+++ popovle1_sudoku.rkt
@@ -18,3 +18,28 @@
   (if (<= n (* i i))
       i
       (my-fake-sqrt-aux n (+ i 1))))
+
+(define (my-list-ref lst i)
+  (if (= i 1)
+      (car lst)
+      (my-list-ref (cdr lst) (- i 1))))
+
+(define (my-get-row mtx y)
+  (my-list-ref mtx y))
+
+(define (my-get-column mtx x)
+  (map (lambda (lst) (my-list-ref lst x)) mtx))
+
+(define (my-get-cell mtx x y)
+  (my-list-ref (my-get-row mtx y) x))
+
+(define mtx-test
+  '((4 0 1 6 0 8 3 0 0)
+    (6 0 0 1 9 3 5 0 0)
+    (3 9 8 0 2 0 0 0 0)
+    (7 0 0 8 0 0 0 2 0)
+    (0 0 5 0 3 6 9 8 7)
+    (0 8 0 0 1 2 0 6 5)
+    (0 3 9 2 0 0 8 1 6)
+    (8 4 0 0 6 1 0 5 9)
+    (1 7 0 9 0 0 0 0 4)))
